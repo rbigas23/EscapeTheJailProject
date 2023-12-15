d8f942852165338c21735ebfd2d6f2c21576f78e
roc <2023_roc.bigas@iticbcn.cat>
2023-12-15 19:32:38 +0100

    Enlaces de actividades

--- app/src/main/java/com/roc/escapethejail/core_activity.kt
+++ app/src/main/java/com/roc/escapethejail/core_activity.kt
@@ -1,36 +1,38 @@
 package com.roc.escapethejail
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 
-class core_activity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class core_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.core)
 
         val start = findViewById<Button>(R.id.start)
-        start.setOnClickListener{
-            setContentView(R.layout.levels)
+        start.setOnClickListener {
+            val intent = Intent(this, core_activity::class.java)  // CAMBIAR LA CLASE
+            startActivity(intent)
         }
 
         val social = findViewById<ImageButton>(R.id.social)
         social.setOnClickListener {
-            setContentView(R.layout.social)
+            val intent = Intent(this, social_activity::class.java)
+            startActivity(intent)
         }
 
         val settings = findViewById<ImageButton>(R.id.settings)
         settings.setOnClickListener {
-            setContentView(R.layout.settings)
+            val intent = Intent(this, core_activity::class.java)  // CAMBIAR LA CLASE
+            startActivity(intent)
         }
 
         val leader_boards = findViewById<ImageButton>(R.id.leader_boards)
         leader_boards.setOnClickListener {
-            setContentView(R.layout.leaderboards)
+            val intent = Intent(this, core_activity::class.java)  // CAMBIAR LA CLASE
+            startActivity(intent)
         }
-
     }
 }
