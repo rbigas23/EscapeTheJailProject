6f5b5879f7eb9ead69e536fdcac8abc96dd740ca
roc <2023_roc.bigas@iticbcn.cat>
2023-12-17 21:04:40 +0100

    Comentarios explicativos

--- app/src/main/java/com/roc/escapethejail/core_activity.kt
+++ app/src/main/java/com/roc/escapethejail/core_activity.kt
@@ -7,19 +7,19 @@ import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class core_activity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {  // Dentro del onCreate definimos los listeners
         super.onCreate(savedInstanceState)
         setContentView(R.layout.core)
 
-        findViewById<Button>(R.id.start).setOnClickListener {
-            val intent = Intent(this, levels_activity::class.java)
-            startActivity(intent)
-        }
+        findViewById<Button>(R.id.start).setOnClickListener {//Buscas el botón por id y defines un listener
+            val intent = Intent(this, levels_activity::class.java) // Creas un intent con el contexto de esta clase
+            startActivity(intent)                                               // y la actividad que vas a abrir al apretar el boton
+        } // Me acabo de dar cuenta de que la variable no es necesaria, se puede poner dentro directamente
 
         findViewById<ImageButton>(R.id.social).setOnClickListener {
-            val intent = Intent(this, social_activity::class.java)
+            val intent = Intent(this, social_activity::class.java)  // Y bueno vas metiendo todos los listeners que quieras
             startActivity(intent)
-        }
+        } // Mirate la social_activity que ahí te explico como se hace lo del return y tal
 
         findViewById<ImageButton>(R.id.settings).setOnClickListener {
             val intent = Intent(this, settings_activity::class.java)
--- app/src/main/java/com/roc/escapethejail/social_activity.kt
+++ app/src/main/java/com/roc/escapethejail/social_activity.kt
@@ -6,11 +6,11 @@ import android.os.Bundle
 import android.widget.Button
 
 class social_activity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { // Aqui también definimos todito dentro del onCreate
         super.onCreate(savedInstanceState)
         setContentView(R.layout.social)
 
-        findViewById<Button>(R.id.add_friend_button).setOnClickListener {
+        findViewById<Button>(R.id.add_friend_button).setOnClickListener { // Esto es otro listener más para ir a add_friend
             val intent = Intent(this, add_friend_activity::class.java)
             startActivity(intent)
         }
@@ -19,6 +19,11 @@ class social_activity : AppCompatActivity() {
             val intent = Intent(this, friend_list_activity::class.java)
             startActivity(intent)
         }
+
+        // Esta siguiente línea es lo del return. Basicamente le estoy diciento que el return sea como apretar
+        // la flecha de back del movil. Lo bueno es que como estamos iniciando actividades en vez de mostrar el
+        // contenido y ya, la flecha de tirar para atrás del móvil funciona "nativamente", y solo queda implementar
+        // el return, como hemos hecho aquií debajo:
 
         findViewById<Button>(R.id.return_social).setOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
