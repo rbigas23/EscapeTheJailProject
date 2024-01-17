509ca7d6880fbd3a93acb3c4cdf24f3e2ee30ccd
tseOrlando <20sw@GMAIL.COM>
2024-01-17 19:03:27 +0100

    db

---
+++ app/src/main/java/com/roc/escapethejail/jail_db.kt
@@ -0,0 +1,22 @@
+package com.roc.escapethejail
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+class jail_db (context: Context?, name: String?, factory: SQLiteDatabase.CursorFactory?, version: Int) : SQLiteOpenHelper(context, name, factory, version)
+{
+    private val user_table = "CREATE TABLE Users" +
+            "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "USER TEXT," +
+            "PASSWORD TEXT," +
+            "EMAIL TEXT)"
+    override fun onCreate(database: SQLiteDatabase?)
+    {
+        database?.execSQL(user_table)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int)
+    {
+        TODO("Not yet implemented")
+    }
+}
