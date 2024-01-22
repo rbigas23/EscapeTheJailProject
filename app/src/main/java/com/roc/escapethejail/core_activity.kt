8f7d77e985653fd14848c1b8968b2c3547ed432b
roc <2023_roc.bigas@iticbcn.cat>
2024-01-22 16:59:45 +0100

    Foldable menu arreeglado

--- app/src/main/java/com/roc/escapethejail/core_activity.kt
+++ app/src/main/java/com/roc/escapethejail/core_activity.kt
@@ -2,6 +2,8 @@ package com.roc.escapethejail
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
@@ -24,5 +26,21 @@ class core_activity : AppCompatActivity()
 
         findViewById<ImageButton>(R.id.leader_boards).setOnClickListener()
         {startActivity(Intent(this, leader_boards_activity::class.java)).apply {}}
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.foldable_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.foldable_menu_home->{startActivity(Intent(this, core_activity::class.java))}
+            R.id.foldable_menu_settings->{startActivity(Intent(this, settings_activity::class.java))}
+            R.id.foldable_menu_social->{startActivity(Intent(this, social_activity::class.java))}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
