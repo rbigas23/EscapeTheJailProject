179dfe5769a856278ba82e705bb394e701476cc3
tseOrlando <20sw@GMAIL.COM>
2024-05-15 19:54:53 +0200

    data monitor finished

--- app/src/main/java/com/roc/escapethejail/monitor_activity.kt
+++ app/src/main/java/com/roc/escapethejail/monitor_activity.kt
@@ -3,6 +3,8 @@ package com.roc.escapethejail
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import java.time.LocalDate
@@ -12,6 +14,8 @@ import java.time.format.DateTimeFormatter
 class monitor_activity : AppCompatActivity()
 {
     lateinit var amount_of_users_registered : TextView
+    lateinit var info_button : Button
+    lateinit var refresh_button : Button
     var once : Boolean = false
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?)
@@ -20,13 +24,21 @@ class monitor_activity : AppCompatActivity()
         setContentView(R.layout.monitor)
 
         amount_of_users_registered = findViewById<TextView>(R.id.monitor_users_registered)
+        info_button = findViewById<Button>(R.id.monitor_info)
+        refresh_button = findViewById<Button>(R.id.monitor_refresh)
+
+        info_button.setOnClickListener() { utils.send_toast("This data is collected when logged in, it's used to show activity to the public", this); }
+        refresh_button.setOnClickListener() { refresh_gui_count()  }
 
         if (!once)
         {
-            amount_of_users_registered.setText(refresh_count().toString())
+            refresh_gui_count()
             once = true
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun refresh_gui_count() { amount_of_users_registered.setText(refresh_count().toString())}
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun refresh_count(count: Long = 0L): Long
@@ -35,19 +47,17 @@ class monitor_activity : AppCompatActivity()
         val oneMonthAgoFormatted = oneMonthAgo.format(DateTimeFormatter.ISO_LOCAL_DATE)
 
         val db = jail_db(this, "sessions", null, 1).writableDatabase
-        var totalSessions = 0L
+        var total_sessions = 0L
 
         db.rawQuery(
             "SELECT SESSION_COUNT FROM sessions WHERE DATE_ADDED >= ?",
             arrayOf(oneMonthAgoFormatted)
         ).use { cursor ->
             while (cursor.moveToNext())
-                totalSessions += cursor.getLong(cursor.getColumnIndexOrThrow("SESSION_COUNT"))
+                total_sessions += cursor.getLong(cursor.getColumnIndexOrThrow("SESSION_COUNT"))
         }
 
-        utils.send_toast("returning refresh $count $totalSessions", this)
-
-        return count + totalSessions
+        return count + total_sessions
     }
 
 }
